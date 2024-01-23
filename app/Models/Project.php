898583a566b2d62a9844b022b33d7989dfb75b3e
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-23 15:53:57 +0100

    add: ProjectTableSeeder with Faker

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -13,7 +13,7 @@ class Project extends Model
 
     public function setTitleAttribute($value)
     {
-        $this->attribute['title']=$value;
-        $this->attribute['slug']=Str::slug($value);
+        $this->attributes["title"] = $value;
+        $this->attributes["slug"]=Str::slug($value);
     }
 }
