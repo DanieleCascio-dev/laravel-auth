6001234217e4ab8fece1b23e54a3e78f0d80f580
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-23 17:08:42 +0100

    add: create method

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -3,6 +3,21 @@
 @section('content')
   <div class="container">
     <h2>Create a new project</h2>
-    <form action=""></form>
+
+    <form class="col-6" action="{{ route('admin.projects.store') }}" method="POST">
+      @csrf
+
+      <div class="mb-3">
+        <label for="title" class="form-label">Title</label>
+        <input required type="text" class="form-control" id="title" name="title">
+      </div>
+
+      <div class="mb-3">
+        <label for="description" class="form-label">Description</label>
+        <textarea class="form-control" id="description" rows="2" name="description"></textarea>
+      </div>
+
+        <button class="btn btn-success">Save</button>
+    </form>
   </div>
 @endsection
