ff4970eb93ecc10196405c0db5d6aad5642ee2a9
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-23 15:23:15 +0100

    add: slug muator for Title

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Support\Str;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    
+
+    public function setTitleAttribute($value)
+    {
+        $this->attribute['title']=$value;
+        $this->attribute['slug']=Str::slug($value);
+    }
 }
